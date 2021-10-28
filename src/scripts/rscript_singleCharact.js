800fc3bf23361448dfd2419df7918ea383a289ee
René Modia <renelog.modia@gmail.com>
2021-10-28 14:53:57 +0200

    add delete sweetAlet

--- src/scripts/rscript_singleCharact.js
+++ src/scripts/rscript_singleCharact.js
@@ -3,10 +3,91 @@ let btnNext = document.querySelector("#next-Charcter")
 let btnPrevious = document.querySelector("#previous-Charcter")
 let singleImg = document.querySelector(".singleImg")
 
+let characterslist
+var actualCharacter=0;
+let lastCharacter=0;
+let firstCharacter=0;
+
 let charactId = localStorage["stored"];
 
+let catchCurrentIndex = async() => { //Catch index of the current character when the page is loaded or reloaded
+
+    let res = await fetch("https://character-database.becode.xyz/characters");
+    let charact = await res.json();
+
+    charact.forEach(el => {
+        if (el.id === charactId){
+            actualCharacter = charact.indexOf(el)
+        }
+    });
+}
+catchCurrentIndex()
+
+let functLastCharacter = async() => { //function to get the index of last character
+    try {
+        let response = await fetch("https://character-database.becode.xyz/characters");
+        characterslist = await response.json();
+        lastCharacter = characterslist.length-1
+        
+    } catch (error) {
+        Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+    }
+
+
+}
+functLastCharacter()
+
+//Next and previous character----------------------------------------------------------------
+
+
+const funcNextCharact = ()=>{ // Next character function------------------------------------------
+    actualCharacter += 1
+
+    if (actualCharacter > lastCharacter){
+        actualCharacter=lastCharacter
+        Swal.fire("This is the last character","","info")
+    }else{
+        charactId = characterslist[actualCharacter].id;
+        localStorage["stored"] = characterslist[actualCharacter].id; //Updating the value of local storage
+
+        document.querySelector('.singleImg').src = "data:image/png;base64," + characterslist[actualCharacter].image;
+        document.querySelector('.singleCharacterName').innerHTML = characterslist[actualCharacter].name;
+        document.querySelector('.singleShortDescription').innerHTML = characterslist[actualCharacter].shortDescription;
+        document.querySelector('.singleLongDescription').innerHTML = characterslist[actualCharacter].description;
+
+    }
+}
+
+btnNext.addEventListener("click",()=>{ //On click go to the Next character------------------
+    funcNextCharact()
+})
+
+btnPrevious.addEventListener("click",()=>{ //Go to the Previous character------------------
+
+    actualCharacter -= 1
+
+    if (actualCharacter < 0){
+        actualCharacter=0
+        Swal.fire("This is the first character","","info")
+    }else{
+        charactId = characterslist[actualCharacter].id;
+        localStorage["stored"] = characterslist[actualCharacter].id; //Updating the value of local storage
+
+        document.querySelector('.singleImg').src = "data:image/png;base64," + characterslist[actualCharacter].image;
+        document.querySelector('.singleCharacterName').innerHTML = characterslist[actualCharacter].name;
+        document.querySelector('.singleShortDescription').innerHTML = characterslist[actualCharacter].shortDescription;
+        document.querySelector('.singleLongDescription').innerHTML = characterslist[actualCharacter].description;
+
+    }
+})
+
+
+
+//Delete character-------------------------------------------------------
+
 btnDelete.addEventListener("click",async()=>{
-   
 
     Swal.fire({
         icon: 'question',
@@ -45,57 +126,12 @@ btnDelete.addEventListener("click",async()=>{
       .then((result)=>{
         if (result.isConfirmed) {
             Swal.fire('The character has been deleted!', '', 'success')
+            functLastCharacter()
+            funcNextCharact()
+
           } else if (result.isDenied) {
             Swal.fire('The character is not deleted', '', 'info')
           }
       })
 })
 
-//Next and previous character---------------------------
-let characterslist
-let actualCharacter=0;
-let lastCharacter=0;
-let firstCharacter=0;
-
-let functCharacters = async() => {
-    if (characterId != 0) {
-    let response = await fetch("https://character-database.becode.xyz/characters");
-    characterslist = await response.json();
-    lastCharacter = characterslist.length-1
-
-}
-else {}
-}
-functCharacters()
-
-btnNext.addEventListener("click",()=>{ //Go to the Next character------------------
-
-    actualCharacter++
-
-    if (actualCharacter > lastCharacter){
-        actualCharacter=lastCharacter
-        Swal.fire("This is the last character","","info")
-    }else{
-        document.querySelector('.singleImg').src = "data:image/png;base64," + characterslist[actualCharacter].image;
-        document.querySelector('.singleCharacterName').innerHTML = characterslist[actualCharacter].name;
-        document.querySelector('.singleShortDescription').innerHTML = characterslist[actualCharacter].shortDescription;
-        document.querySelector('.singleLongDescription').innerHTML = characterslist[actualCharacter].description;
-
-    }
-})
-
-btnPrevious.addEventListener("click",()=>{ //Go to the Previous character------------------
-
-    actualCharacter--
-
-    if (actualCharacter < 0){
-        actualCharacter=0
-        Swal.fire("This is the first character","","info")
-    }else{
-        document.querySelector('.singleImg').src = "data:image/png;base64," + characterslist[actualCharacter].image;
-        document.querySelector('.singleCharacterName').innerHTML = characterslist[actualCharacter].name;
-        document.querySelector('.singleShortDescription').innerHTML = characterslist[actualCharacter].shortDescription;
-        document.querySelector('.singleLongDescription').innerHTML = characterslist[actualCharacter].description;
-
-    }
-})
